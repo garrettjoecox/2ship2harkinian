5a2f02627df75f8f1f67e2c5b78942e6922bea80
Travis Smith <balloondude2@gmail.com>
2024-10-11 23:11:03 -0600

    use vb_play_transition_cs

--- mm/2s2h/Rando/ActorBehavior/ObjUm.cpp
+++ mm/2s2h/Rando/ActorBehavior/ObjUm.cpp
@@ -5,9 +5,7 @@ extern "C" {
 #include "variables.h"
 }
 
-// Registers hooks in both z_obj_um.c and z_en_ma_yto.c
 void Rando::ActorBehavior::InitObjUmBehavior() {
-    static uint32_t onActorUpdateHookId = 0;
     static uint32_t shouldHookId1 = 0;
     static uint32_t shouldHookId2 = 0;
     GameInteractor::Instance->UnregisterGameHookForID<GameInteractor::ShouldVanillaBehavior>(shouldHookId1);
@@ -22,19 +20,15 @@ void Rando::ActorBehavior::InitObjUmBehavior() {
 
     shouldHookId1 =
         REGISTER_VB_SHOULD(VB_HAVE_ROMANI_MASK, { *should = RANDO_SAVE_CHECKS[RC_MILK_RUN_REWARD].obtained; });
-    shouldHookId2 = REGISTER_VB_SHOULD(VB_WON_MILK_RUN, {
+    shouldHookId2 = REGISTER_VB_SHOULD(VB_PLAY_TRANSITION_CS, {
         if (RANDO_SAVE_CHECKS[RC_MILK_RUN_REWARD].obtained) {
             return;
         }
-
-        *should = false;
-        // Combination of things set between obj_um.c and en_ma_yto.c
-        gSaveContext.nextCutsceneIndex = 0;
-        gPlayState->nextEntrance = ENTRANCE(TERMINA_FIELD, 13);
-        gPlayState->transitionType = TRANS_TYPE_80;
-        gSaveContext.nextTransitionType = TRANS_TYPE_FADE_WHITE;
-        gPlayState->transitionTrigger = TRANS_TRIGGER_START;
-        gSaveContext.save.time += CLOCK_TIME(1, 0) + 2;
+        if (!(gSaveContext.save.cutsceneIndex == 0xFFF3 && gSaveContext.save.entrance == ENTRANCE(TERMINA_FIELD, 0))) {
+            return;
+        }
+        gSaveContext.save.cutsceneIndex = 0;
+        gSaveContext.save.entrance = ENTRANCE(TERMINA_FIELD, 13);
         if (!RANDO_SAVE_CHECKS[RC_MILK_RUN_REWARD].eligible) {
             RANDO_SAVE_CHECKS[RC_MILK_RUN_REWARD].eligible = true;
         }
