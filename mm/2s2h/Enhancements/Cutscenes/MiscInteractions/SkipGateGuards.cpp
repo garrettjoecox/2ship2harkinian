e2d2a896aa07f49dade23ca856d35d70e818a9fd
Travis <balloondude2@gmail.com>
2024-12-19 16:30:54 -0700

    clang format

--- mm/2s2h/Enhancements/Cutscenes/MiscInteractions/SkipGateGuards.cpp
+++ mm/2s2h/Enhancements/Cutscenes/MiscInteractions/SkipGateGuards.cpp
@@ -10,8 +10,7 @@ extern "C" {
 #define CVAR CVarGetInteger(CVAR_NAME, 0)
 
 void RegisterSkipGateGuards() {
-    COND_ID_HOOK(OnActorInit, ACTOR_EN_STOP_HEISHI, CVAR,
-                 [](Actor* actor) { SET_WEEKEVENTREG(WEEKEVENTREG_12_20); });
+    COND_ID_HOOK(OnActorInit, ACTOR_EN_STOP_HEISHI, CVAR, [](Actor* actor) { SET_WEEKEVENTREG(WEEKEVENTREG_12_20); });
 }
 
 static RegisterShipInitFunc initFunc(RegisterSkipGateGuards, { CVAR_NAME });
