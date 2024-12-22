15bc77b0fa9f21add15f447187de498378438420
sitton76 <58642183+sitton76@users.noreply.github.com>
2024-12-22 07:10:23 -0600

    clang plz

--- mm/2s2h/Rando/ActorBehavior/BgGoronOyu.cpp
+++ mm/2s2h/Rando/ActorBehavior/BgGoronOyu.cpp
@@ -4,8 +4,8 @@ extern "C" {
 void Flags_SetRandoInf(s32 flag);
 }
 
-// When BgGoronOyu is initialized this means the player should now be considered to have access to Hot Spring Water. This flag
-// is purely to drive the live updating check tracker, it has no effect on actual gameplay.
+// When BgGoronOyu is initialized this means the player should now be considered to have access to Hot Spring Water.
+// This flag is purely to drive the live updating check tracker, it has no effect on actual gameplay.
 void Rando::ActorBehavior::InitGbGoronOyuBehavior() {
     COND_ID_HOOK(OnActorInit, ACTOR_BG_GORON_OYU, IS_RANDO, [](Actor* actor) {
         if (!RANDO_ACCESS[RANDO_ACCESS_HOT_SPRING_WATER]) {
