eee4ff2490b5a20e773f7d459a31bb1436bf82fa
Caladius <Caladius@users.noreply.github.com>
2024-12-31 17:38:10 -0500

    remove Letter to Mama Starting item

--- mm/2s2h/Rando/Logic/GlitchlessLogic.cpp
+++ mm/2s2h/Rando/Logic/GlitchlessLogic.cpp
@@ -54,7 +54,7 @@ void ApplyGlitchlessLogicToSaveContext() {
     // section.
     std::vector<RandoItemId> startingItems = {
         RI_SONG_STORMS, RI_MASK_COUPLE,    RI_SONG_LULLABY,   RI_DEKU_STICK,
-        RI_DEKU_NUT,    RI_PICTOGRAPH_BOX, RI_LETTER_TO_MAMA,
+        RI_DEKU_NUT,    RI_PICTOGRAPH_BOX,
     };
 
     for (RandoItemId randoItemId : startingItems) {
