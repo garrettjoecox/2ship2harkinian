70571a29b5c9e9f0d6954496df12803f90feb584
sitton76 <58642183+sitton76@users.noreply.github.com>
2025-01-03 01:54:29 -0600

    Fixed GI model crashing game.

--- mm/2s2h/Rando/DrawItem.cpp
+++ mm/2s2h/Rando/DrawItem.cpp
@@ -99,6 +99,9 @@ void DrawSong(RandoItemId randoItemId) {
         case RI_SONG_ELEGY:
             gDPSetEnvColor(POLY_XLU_DISP++, 255, 98, 0, 255);
             break;
+        case RI_SONG_LULLABY_INTRO:
+            gDPSetEnvColor(POLY_XLU_DISP++, 255, 194, 194, 255);
+            break;
         case RI_SONG_LULLABY:
             gDPSetEnvColor(POLY_XLU_DISP++, 255, 100, 100, 255);
             break;
@@ -192,6 +195,7 @@ void Rando::DrawItem(RandoItemId randoItemId) {
         case RI_SONG_SOARING:
         case RI_SONG_SONATA:
         case RI_SONG_ELEGY:
+        case RI_SONG_LULLABY_INTRO:
         case RI_SONG_LULLABY:
         case RI_SONG_OATH:
         case RI_SONG_EPONA:
