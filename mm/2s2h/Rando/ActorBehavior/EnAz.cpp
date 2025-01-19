4c676ee655d0a0f689a80e91f7d30bc1e1f5ad4f
Eblo <7004497+Eblo@users.noreply.github.com>
2025-01-18 20:01:35 -0500

    Allow repeat rupee rewards for beaver races

--- mm/2s2h/Rando/ActorBehavior/EnAz.cpp
+++ mm/2s2h/Rando/ActorBehavior/EnAz.cpp
@@ -10,7 +10,7 @@ void Rando::ActorBehavior::InitEnAzBehavior() {
     COND_VB_SHOULD(VB_GIVE_ITEM_FROM_OFFER, IS_RANDO, {
         GetItemId* getItemId = va_arg(args, GetItemId*);
         Actor* actor = va_arg(args, Actor*);
-        if (actor->id == ACTOR_EN_AZ) { // Beaver Bro
+        if (actor->id == ACTOR_EN_AZ && *getItemId > GI_RUPEE_PURPLE) { // Beaver Bro non-repeat reward
             Player* player = GET_PLAYER(gPlayState);
             *should = false;
             // Force advance textbox
