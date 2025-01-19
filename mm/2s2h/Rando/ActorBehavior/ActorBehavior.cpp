5d18ee27acde702f40416612b791c2f68aa4e4ec
Caladius <Caladius@users.noreply.github.com>
2025-01-19 17:09:01 -0500

    Shuffles Zora Hall Scene Lights

--- mm/2s2h/Rando/ActorBehavior/ActorBehavior.cpp
+++ mm/2s2h/Rando/ActorBehavior/ActorBehavior.cpp
@@ -84,6 +84,7 @@ void Rando::ActorBehavior::OnFileLoad() {
     Rando::ActorBehavior::InitEnTotoBehavior();
     Rando::ActorBehavior::InitEnYbBehavior();
     Rando::ActorBehavior::InitEnZogBehavior();
+    Rando::ActorBehavior::InitEnZotBehavior();
     Rando::ActorBehavior::InitItemBHeartBehavior();
     Rando::ActorBehavior::InitObjKibakoBehavior();
     Rando::ActorBehavior::InitObjMoonStoneBehavior();
--- mm/2s2h/Rando/ActorBehavior/ActorBehavior.h
+++ mm/2s2h/Rando/ActorBehavior/ActorBehavior.h
@@ -62,6 +62,7 @@ void InitEnSyatekiManBehavior();
 void InitEnTotoBehavior();
 void InitEnYbBehavior();
 void InitEnZogBehavior();
+void InitEnZotBehavior();
 void InitItemBHeartBehavior();
 void InitObjKibakoBehavior();
 void InitObjMoonStoneBehavior();
