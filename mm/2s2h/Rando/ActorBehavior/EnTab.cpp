193e8a6d27710b49833d068a6c9a38500cd9ad02
Josh Sanchez <joshuaesanch@gmail.com>
2025-02-04 16:33:57 -0600

    Add dialogue state tracking to EnTab ActorBehavior

--- mm/2s2h/Rando/ActorBehavior/EnTab.cpp
+++ mm/2s2h/Rando/ActorBehavior/EnTab.cpp
@@ -4,9 +4,103 @@
 extern "C" {
 #include "variables.h"
 #include "overlays/actors/ovl_En_Tab/z_en_tab.h"
+
+MsgScript D_80BE19A0[];
+s32 func_80BE0D38(Actor* thisx, PlayState* play);  // Standard dialogue mscript callback that does bottle check
 }
 
-void EnTab_OnOpenText(u16* textId, bool* loadFromMessageTable) {
+enum TabDialogueState {
+    TAB_D_IDLE,
+    TAB_D_INITIAL_DIALOGUE,
+    TAB_D_SHOP_DIALOGUE,
+};
+
+MsgScript standardDialogueOverrideScript[] = {
+    /* 0x0000 0x03 */ MSCRIPT_BEGIN_TEXT(0x2B0A),
+    /* 0x0003 0x01 */ MSCRIPT_AWAIT_TEXT(),
+    /* 0x0004 0x07 */ MSCRIPT_BRANCH_ON_TEXT_CHOICE(0x006D - 0x000B, 0x0, 0x006D - 0x000B),
+    /* 0x000B 0x01 */ MSCRIPT_PLAY_DECIDE(),
+    /* 0x000C 0x03 */ MSCRIPT_BRANCH_ON_CALLBACK_2(0x0014 - 0x000F),
+    /* 0x000F 0x03 */ MSCRIPT_BEGIN_TEXT(0x2B0E),
+    /* 0x0012 0x01 */ MSCRIPT_AWAIT_TEXT(),
+    /* 0x0013 0x01 */ MSCRIPT_DONE(),
+
+    /* 0x0014 0x03 */ MSCRIPT_BEGIN_TEXT(0x2B0B),
+    /* 0x0017 0x01 */ MSCRIPT_AWAIT_TEXT(),
+    /* 0x0018 0x07 */ MSCRIPT_BRANCH_ON_TEXT_CHOICE(0x0, 0x0031 - 0x001F, 0x0043 - 0x001F),
+    /* 0x001F 0x03 */ MSCRIPT_BRANCH_ON_CALLBACK_2(0x0029 - 0x0022),
+    /* 0x0022 0x03 */ MSCRIPT_BEGIN_TEXT(0x2B0E),
+    /* 0x0025 0x01 */ MSCRIPT_AWAIT_TEXT(),
+    /* 0x0026 0x03 */ MSCRIPT_JUMP(0x0048 - 0x0029),
+    /* 0x0029 0x05 */ MSCRIPT_BRANCH_ON_RUPEES(0x0014, 0x005E - 0x002E),
+    /* 0x002E 0x03 */ MSCRIPT_JUMP(0x0049 - 0x0031),
+    /* 0x0031 0x03 */ MSCRIPT_BRANCH_ON_CALLBACK_2(0x003B - 0x0034),
+    /* 0x0034 0x03 */ MSCRIPT_BEGIN_TEXT(0x2B0E),
+    /* 0x0037 0x01 */ MSCRIPT_AWAIT_TEXT(),
+    /* 0x0038 0x03 */ MSCRIPT_JUMP(0x0048 - 0x003B),
+    /* 0x003B 0x05 */ MSCRIPT_BRANCH_ON_RUPEES(0x00C8, 0x004F - 0x0040) ,
+    /* 0x0040 0x03 */ MSCRIPT_JUMP(0x0049 - 0x0043),
+    /* 0x0043 0x01 */ MSCRIPT_PLAY_CANCEL(),
+    /* 0x0044 0x03 */ MSCRIPT_BEGIN_TEXT(0x2B0D),
+    /* 0x0047 0x01 */ MSCRIPT_AWAIT_TEXT(),
+    /* 0x0048 0x01 */ MSCRIPT_DONE(),
+
+    /* 0x0049 0x01 */ MSCRIPT_PLAY_ERROR(),
+    /* 0x004A 0x03 */ MSCRIPT_BEGIN_TEXT(0x2B0C),
+    /* 0x004D 0x01 */ MSCRIPT_AWAIT_TEXT(),
+    /* 0x004E 0x01 */ MSCRIPT_DONE(),
+
+    /* 0x004F 0x01 */ MSCRIPT_PLAY_DECIDE(),
+    /* 0x0050 0x01 */ MSCRIPT_CLOSE_TEXT(),
+    /* 0x0051 0x03 */ MSCRIPT_CHANGE_RUPEES(0xFF38),
+    /* 0x0054 0x05 */ MSCRIPT_OFFER_ITEM(GI_CHATEAU, 0x0),
+    /* 0x0059 0x03 */ MSCRIPT_COLLECT_SET(0x0091),
+    /* 0x005C 0x01 */ MSCRIPT_AWAIT_TEXT(),
+    /* 0x005D 0x01 */ MSCRIPT_DONE(),
+
+    /* 0x005E 0x01 */ MSCRIPT_PLAY_DECIDE(),
+    /* 0x005F 0x01 */ MSCRIPT_CLOSE_TEXT(),
+    /* 0x0060 0x03 */ MSCRIPT_CHANGE_RUPEES(0xFFEC),
+    /* 0x0063 0x05 */ MSCRIPT_OFFER_ITEM(GI_MILK, 0x0),
+    /* 0x0068 0x03 */ MSCRIPT_COLLECT_SET(0x0092),
+    /* 0x006B 0x01 */ MSCRIPT_AWAIT_TEXT(),
+    /* 0x006C 0x01 */ MSCRIPT_DONE(),
+
+    /* 0x006D 0x01 */ MSCRIPT_PLAY_DECIDE(),
+    /* 0x006E 0x05 */ MSCRIPT_BRANCH_ON_WEEK_EVENT_REG(0x34, 0x01, 0x0078 - 0x0073),
+    /* 0x0073 0x03 */ MSCRIPT_BEGIN_TEXT(0x2B0F),
+    /* 0x0076 0x01 */ MSCRIPT_AWAIT_TEXT(),
+    /* 0x0077 0x01 */ MSCRIPT_DONE(),
+
+    /* 0x0078 0x03 */ MSCRIPT_BEGIN_TEXT(0x2B11),
+    /* 0x007B 0x01 */ MSCRIPT_AWAIT_TEXT(),
+    /* 0x007C 0x01 */ MSCRIPT_DONE(),
+};
+
+static TabDialogueState tabDialogueState = TAB_D_IDLE;
+
+int32_t EnTab_OverrideBottleCheckCallback(Actor* thisx, PlayState* play) {
+    // Should always return true - as if the player always has an empty bottle!
+    // See func_80BE0D38 in z_en_tab.c
+    return true;   
+}
+
+void EnTab_UpdateDialogueState(u16* textId, bool* loadFromMessageTable) {
+    switch (*textId) 
+    {
+    case 0x2B0A:
+        tabDialogueState = TAB_D_INITIAL_DIALOGUE;
+        break;
+    case 0x2B0B:
+        tabDialogueState = TAB_D_SHOP_DIALOGUE;
+        LUSLOG_DEBUG("it's shopping time!");
+        break;
+    default:
+        tabDialogueState = TAB_D_IDLE;
+    }
+}
+
+void EnTab_OnOpenShopText(u16* textId, bool* loadFromMessageTable) {
     RandoSaveCheck milkPurchaseCheck = RANDO_SAVE_CHECKS[RC_MILK_BAR_PURCHASE_MILK];
     RandoSaveCheck chateauPurchaseCheck = RANDO_SAVE_CHECKS[RC_MILK_BAR_PURCHASE_CHATEAU];
 
@@ -120,5 +214,13 @@ void Rando::ActorBehavior::InitEnTabBehavior() {
         }
     });
 
-    COND_ID_HOOK(OnOpenText, 0x2B0B, IS_RANDO, EnTab_OnOpenText);
+    COND_ID_HOOK(OnOpenText, 0x2B0B, IS_RANDO, EnTab_OnOpenShopText);
+
+    COND_ID_HOOK(OnOpenText, 0x2B0A, IS_RANDO, EnTab_UpdateDialogueState);
+    COND_ID_HOOK(OnOpenText, 0x2B0B, IS_RANDO, EnTab_UpdateDialogueState);
+    COND_ID_HOOK(OnOpenText, 0x2B0C, IS_RANDO, EnTab_UpdateDialogueState);
+    COND_ID_HOOK(OnOpenText, 0x2B0D, IS_RANDO, EnTab_UpdateDialogueState);
+    COND_ID_HOOK(OnOpenText, 0x2B0E, IS_RANDO, EnTab_UpdateDialogueState);
+    COND_ID_HOOK(OnOpenText, 0x2B0F, IS_RANDO, EnTab_UpdateDialogueState);
+    COND_ID_HOOK(OnOpenText, 0x2B11, IS_RANDO, EnTab_UpdateDialogueState);
 }
