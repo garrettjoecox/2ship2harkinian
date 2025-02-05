2c2396d67d97e04452d53e69d05d1aff467c3aa4
Josh Sanchez <joshuaesanch@gmail.com>
2025-02-04 22:09:52 -0600

    Remove unused reference to EnTab actor script that was crashing builds on Linux/Mac

--- mm/2s2h/Rando/ActorBehavior/EnTab.cpp
+++ mm/2s2h/Rando/ActorBehavior/EnTab.cpp
@@ -5,7 +5,6 @@ extern "C" {
 #include "variables.h"
 #include "overlays/actors/ovl_En_Tab/z_en_tab.h"
 
-MsgScript D_80BE19A0[];
 s32 func_80BE0D38(Actor* thisx, PlayState* play); // Standard dialogue mscript callback that does bottle check
 }
 
