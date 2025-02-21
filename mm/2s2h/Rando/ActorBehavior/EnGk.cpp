3978e501c8568db9b979d3b325381d9328b61118
Josh Sanchez <joshuaesanch@gmail.com>
2025-02-20 22:55:22 -0600

    Override Goron Lullaby cutscene skip behavior to instead grant rando check reward

--- mm/2s2h/Rando/ActorBehavior/EnGk.cpp
+++ mm/2s2h/Rando/ActorBehavior/EnGk.cpp
@@ -30,6 +30,23 @@ void Rando::ActorBehavior::InitEnGKBehavior() {
         }
     });
 
+    COND_VB_SHOULD(VB_GIVE_ITEM_FROM_GK_LULLABY, IS_RANDO, {
+        // Override vanilla cutscene skip item grant behavior to use rando queue instead
+        *should = false;
+    });
+
+    COND_VB_SHOULD(VB_START_CUTSCENE, IS_RANDO, {
+        s16* csId = va_arg(args, s16*);
+        Actor* actor = va_arg(args, Actor*);
+
+        if (*csId != 9 || actor == NULL || actor->id != ACTOR_EN_GK) {
+            return;
+        }
+
+        SET_WEEKEVENTREG(WEEKEVENTREG_24_80); // Ensure Goron Elder check is available
+        RANDO_SAVE_CHECKS[RC_GORON_SHRINE_FULL_LULLABY].eligible = true;
+    });
+
     // Played Full Lullaby for Baby Goron
     COND_HOOK(OnSceneFlagSet, IS_RANDO, [](s16 sceneId, FlagType flagType, u32 flag) {
         if (sceneId == SCENE_16GORON_HOUSE && flagType == FLAG_CYCL_SCENE_SWITCH && flag == 20) {
