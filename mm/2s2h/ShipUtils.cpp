8c54aa0a5c9bde3c32fb935695b99643f689c406
Garrett <garrettjcox@gmail.com>
2026-07-11 07:00:29 -0500

    Add initial sail implementation

--- mm/2s2h/ShipUtils.cpp
+++ mm/2s2h/ShipUtils.cpp
@@ -421,6 +421,20 @@ std::string GetActorCategoryName(u8 category) {
     return "Unknown";
 }
 
+bool isStringEmpty(std::string str) {
+    // Remove spaces at the beginning of the string
+    std::string::size_type start = str.find_first_not_of(' ');
+    // Remove spaces at the end of the string
+    std::string::size_type end = str.find_last_not_of(' ');
+
+    // Check if the string is empty after stripping spaces
+    if (start == std::string::npos || end == std::string::npos) {
+        return true; // The string is empty
+    } else {
+        return false; // The string is not empty
+    }
+}
+
 extern "C" {
 Vtx sCycleExtraItemVtx[8] = {
     // Left Item
