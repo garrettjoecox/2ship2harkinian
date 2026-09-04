7f6d045855e1b1ef2d2505b2b52fb42ef219c38a
CobraChicken997 <52684366+CobraChicken997@users.noreply.github.com>
2026-09-03 23:15:55 -0230

    Comment out problematic entrance definitions
    Commented out entrance definitions that cause crashes or have specific cutscene notes.

--- mm/include/tables/entrance_table.h
+++ mm/include/tables/entrance_table.h
@@ -19,7 +19,7 @@
 // Mayor's Residence (Ext: 0x00, Ent: 3)
 #define ENTRANCE_MAYORS_RESIDENCE_0     0x0000
 #define ENTRANCE_MAYORS_RESIDENCE_1     0x0010
-#define ENTRANCE_MAYORS_RESIDENCE_2     0x0020
+//#define ENTRANCE_MAYORS_RESIDENCE_2     0x0020 Crashes
 
 // East Clock Town (Ext: 0x69, Ent: 13)
 #define ENTRANCE_EAST_CLOCK_TOWN_0      0xD200
@@ -34,7 +34,7 @@
 #define ENTRANCE_EAST_CLOCK_TOWN_9      0xD290
 #define ENTRANCE_EAST_CLOCK_TOWN_10     0xD2A0
 #define ENTRANCE_EAST_CLOCK_TOWN_11     0xD2B0
-#define ENTRANCE_EAST_CLOCK_TOWN_12     0xD2C0
+//#define ENTRANCE_EAST_CLOCK_TOWN_12     0xD2C0 Crashes
 
 // West Clock Town (Ext: 0x6A, Ent: 10)
 #define ENTRANCE_WEST_CLOCK_TOWN_0      0xD400
@@ -69,26 +69,26 @@
 #define ENTRANCE_SOUTH_CLOCK_TOWN_7     0xD870
 #define ENTRANCE_SOUTH_CLOCK_TOWN_8     0xD880
 #define ENTRANCE_SOUTH_CLOCK_TOWN_9     0xD890
-#define ENTRANCE_SOUTH_CLOCK_TOWN_10    0xD8A0
+// #define ENTRANCE_SOUTH_CLOCK_TOWN_10    0xD8A0 Cutscene from first Song of Time use
 
 // Laundry Pool (Ext: 0x6D, Ent: 3)
 #define ENTRANCE_LAUNDRY_POOL_0         0xDA00
 #define ENTRANCE_LAUNDRY_POOL_1         0xDA10
-#define ENTRANCE_LAUNDRY_POOL_2         0xDA20
+// #define ENTRANCE_LAUNDRY_POOL_2         0xDA20 Crashes
 
 // Clock Tower Interior (Ext: 0x60, Ent: 7)
-#define ENTRANCE_CLOCK_TOWER_INTERIOR_0 0xC000
+// #define ENTRANCE_CLOCK_TOWER_INTERIOR_0 0xC000 First cutscene entering Clock Tower
 #define ENTRANCE_CLOCK_TOWER_INTERIOR_1 0xC010
-#define ENTRANCE_CLOCK_TOWER_INTERIOR_2 0xC020
-#define ENTRANCE_CLOCK_TOWER_INTERIOR_3 0xC030
-#define ENTRANCE_CLOCK_TOWER_INTERIOR_4 0xC040
+// #define ENTRANCE_CLOCK_TOWER_INTERIOR_2 0xC020 // Song of healing Deku Mask cutscene
+// #define ENTRANCE_CLOCK_TOWER_INTERIOR_3 0xC030 // Cutscene with mask salesman about time left to get MM
+// #define ENTRANCE_CLOCK_TOWER_INTERIOR_4 0xC040 // Similar cutscene to first Song of Time use
 #define ENTRANCE_CLOCK_TOWER_INTERIOR_5 0xC050
-#define ENTRANCE_CLOCK_TOWER_INTERIOR_6 0xC060
+// #define ENTRANCE_CLOCK_TOWER_INTERIOR_6 0xC060 // Mask salesman explaining about the power of MM and the imp having it
 
 // Clock Tower Rooftop (Ext: 0x16, Ent: 3)
 #define ENTRANCE_CLOCK_TOWER_ROOFTOP_0  0x2C00
-#define ENTRANCE_CLOCK_TOWER_ROOFTOP_1  0x2C10
-#define ENTRANCE_CLOCK_TOWER_ROOFTOP_2  0x2C20
+// #define ENTRANCE_CLOCK_TOWER_ROOFTOP_1  0x2C10 // After learning song of time from getting ocarina back cutscene
+// #define ENTRANCE_CLOCK_TOWER_ROOFTOP_2  0x2C20 Crashes!
 
 // ========================================================================
 // TERMINA FIELD & OVERWORLD
@@ -104,11 +104,11 @@
 #define ENTRANCE_TERMINA_FIELD_7        0x5470
 #define ENTRANCE_TERMINA_FIELD_8        0x5480
 #define ENTRANCE_TERMINA_FIELD_9        0x5490
-#define ENTRANCE_TERMINA_FIELD_10       0x54A0
+// #define ENTRANCE_TERMINA_FIELD_10       0x54A0 Telescope cutscene
 #define ENTRANCE_TERMINA_FIELD_11       0x54B0
-#define ENTRANCE_TERMINA_FIELD_12       0x54C0
+// #define ENTRANCE_TERMINA_FIELD_12       0x54C0 Mask Salesman cutscene?
 #define ENTRANCE_TERMINA_FIELD_13       0x54D0
-#define ENTRANCE_TERMINA_FIELD_14       0x54E0
+// #define ENTRANCE_TERMINA_FIELD_14       0x54E0 Drawing of Skull Kid on tree ending cutscene
 
 // Road to Southern Swamp (Ext: 0x3D, Ent: 3)
 #define ENTRANCE_ROAD_TO_SOUTHERN_SWAMP_0 0x7A00
@@ -137,7 +137,7 @@
 #define ENTRANCE_ROMANI_RANCH_4         0x6440
 #define ENTRANCE_ROMANI_RANCH_5         0x6450
 #define ENTRANCE_ROMANI_RANCH_6         0x6460
-#define ENTRANCE_ROMANI_RANCH_7         0x6470
+// #define ENTRANCE_ROMANI_RANCH_7         0x6470 // Duplicate as above entrance?
 #define ENTRANCE_ROMANI_RANCH_8         0x6480
 #define ENTRANCE_ROMANI_RANCH_9         0x6490
 #define ENTRANCE_ROMANI_RANCH_10        0x64A0
@@ -153,7 +153,7 @@
 #define ENTRANCE_SOUTHERN_SWAMP_CLEAR_3 0x0C30
 #define ENTRANCE_SOUTHERN_SWAMP_CLEAR_4 0x0C40
 #define ENTRANCE_SOUTHERN_SWAMP_CLEAR_5 0x0C50
-#define ENTRANCE_SOUTHERN_SWAMP_CLEAR_6 0x0C60
+// #define ENTRANCE_SOUTHERN_SWAMP_CLEAR_6 0x0C60 Boat ride at Swamp (cleared)
 #define ENTRANCE_SOUTHERN_SWAMP_CLEAR_7 0x0C70
 #define ENTRANCE_SOUTHERN_SWAMP_CLEAR_8 0x0C80
 #define ENTRANCE_SOUTHERN_SWAMP_CLEAR_9 0x0C90
@@ -166,7 +166,7 @@
 #define ENTRANCE_SOUTHERN_SWAMP_POISON_3 0x8430
 #define ENTRANCE_SOUTHERN_SWAMP_POISON_4 0x8440
 #define ENTRANCE_SOUTHERN_SWAMP_POISON_5 0x8450
-#define ENTRANCE_SOUTHERN_SWAMP_POISON_6 0x8460
+// #define ENTRANCE_SOUTHERN_SWAMP_POISON_6 0x8460 Boat ride at Swamp (Poisoned)
 #define ENTRANCE_SOUTHERN_SWAMP_POISON_7 0x8470
 #define ENTRANCE_SOUTHERN_SWAMP_POISON_8 0x8480
 #define ENTRANCE_SOUTHERN_SWAMP_POISON_9 0x8490
@@ -174,9 +174,9 @@
 
 // Woodfall (Ext: 0x43, Ent: 5)
 #define ENTRANCE_WOODFALL_0             0x8600
-#define ENTRANCE_WOODFALL_1             0x8610
+// #define ENTRANCE_WOODFALL_1             0x8610 Not a good spawn in uncleared WoodFall (above swamp water)
 #define ENTRANCE_WOODFALL_2             0x8620
-#define ENTRANCE_WOODFALL_3             0x8630
+// #define ENTRANCE_WOODFALL_3             0x8630 Also not a good spawn in uncleared WoodFall (above swamp water)
 #define ENTRANCE_WOODFALL_4             0x8640
 
 // Woodfall Temple (Ext: 0x18, Ent: 3)
@@ -261,18 +261,18 @@
 #define ENTRANCE_GORON_VILLAGE_SPRING_0 0x8A00
 #define ENTRANCE_GORON_VILLAGE_SPRING_1 0x8A10
 #define ENTRANCE_GORON_VILLAGE_SPRING_2 0x8A20
-#define ENTRANCE_GORON_VILLAGE_SPRING_3 0x8A30
+// #define ENTRANCE_GORON_VILLAGE_SPRING_3 0x8A30 Lens of Truth Shrine spawn in spring except, there's no shrine lol
 #define ENTRANCE_GORON_VILLAGE_SPRING_4 0x8A40
 
 // Goron Shrine (Ext: 0x2F, Ent: 4)
 #define ENTRANCE_GORON_SHRINE_0         0x5E00
 #define ENTRANCE_GORON_SHRINE_1         0x5E10
-#define ENTRANCE_GORON_SHRINE_2         0x5E20
+// #define ENTRANCE_GORON_SHRINE_2         0x5E20 Learn full Goron Lullaby cutscene
 #define ENTRANCE_GORON_SHRINE_3         0x5E30
 
 // Goron Graveyard (Ext: 0x4B, Ent: 2)
 #define ENTRANCE_GORON_GRAVEYARD_0      0x9600
-#define ENTRANCE_GORON_GRAVEYARD_1      0x9610
+// #define ENTRANCE_GORON_GRAVEYARD_1      0x9610 //Song of healing Goron Mask cutscene
 
 // Mountain Smithy (Ext: 0x29, Ent: 1)
 #define ENTRANCE_MOUNTAIN_SMITHY_0      0x5200
@@ -290,7 +290,7 @@
 #define ENTRANCE_GREAT_BAY_COAST_6      0x6860
 #define ENTRANCE_GREAT_BAY_COAST_7      0x6870
 #define ENTRANCE_GREAT_BAY_COAST_8      0x6880
-#define ENTRANCE_GREAT_BAY_COAST_9      0x6890
+// #define ENTRANCE_GREAT_BAY_COAST_9      0x6890 Zora's Mask Song of Healing cutscene
 #define ENTRANCE_GREAT_BAY_COAST_10     0x68A0
 #define ENTRANCE_GREAT_BAY_COAST_11     0x68B0
 #define ENTRANCE_GREAT_BAY_COAST_12     0x68C0
@@ -319,11 +319,11 @@
 #define ENTRANCE_PIRATES_FORTRESS_7     0x2270
 #define ENTRANCE_PIRATES_FORTRESS_8     0x2280
 #define ENTRANCE_PIRATES_FORTRESS_9     0x2290
-#define ENTRANCE_PIRATES_FORTRESS_10    0x22A0
-#define ENTRANCE_PIRATES_FORTRESS_11    0x22B0
+// #define ENTRANCE_PIRATES_FORTRESS_10    0x22A0 Pirate Telescope
+// #define ENTRANCE_PIRATES_FORTRESS_11    0x22B0 Void out?
 #define ENTRANCE_PIRATES_FORTRESS_12    0x22C0
 #define ENTRANCE_PIRATES_FORTRESS_13    0x22D0
-#define ENTRANCE_PIRATES_FORTRESS_14    0x22E0
+// #define ENTRANCE_PIRATES_FORTRESS_14    0x22E0 Crashes
 
 // Pirates' Fortress Interior (Ext: 0x20, Ent: 16)
 #define ENTRANCE_PIRATES_FORTRESS_INTERIOR_0 0x4000
@@ -337,11 +337,11 @@
 #define ENTRANCE_PIRATES_FORTRESS_INTERIOR_8 0x4080
 #define ENTRANCE_PIRATES_FORTRESS_INTERIOR_9 0x4090
 #define ENTRANCE_PIRATES_FORTRESS_INTERIOR_10 0x40A0
-#define ENTRANCE_PIRATES_FORTRESS_INTERIOR_11 0x40B0
-#define ENTRANCE_PIRATES_FORTRESS_INTERIOR_12 0x40C0
-#define ENTRANCE_PIRATES_FORTRESS_INTERIOR_13 0x40D0
-#define ENTRANCE_PIRATES_FORTRESS_INTERIOR_14 0x40E0
-#define ENTRANCE_PIRATES_FORTRESS_INTERIOR_15 0x40F0
+// #define ENTRANCE_PIRATES_FORTRESS_INTERIOR_11 0x40B0 Out of bounds pirate fight?
+// #define ENTRANCE_PIRATES_FORTRESS_INTERIOR_12 0x40C0 CRASH
+// #define ENTRANCE_PIRATES_FORTRESS_INTERIOR_13 0x40D0 CRASH
+// #define ENTRANCE_PIRATES_FORTRESS_INTERIOR_14 0x40E0 CRASH
+// #define ENTRANCE_PIRATES_FORTRESS_INTERIOR_15 0x40F0 Out of bounds pirate fight 2?
 
 // Pirates' Fortress Exterior (Ext: 0x38, Ent: 7)
 #define ENTRANCE_PIRATES_FORTRESS_EXTERIOR_0 0x7000
@@ -358,8 +358,8 @@
 
 // Great Bay Temple (Ext: 0x46, Ent: 3)
 #define ENTRANCE_GREAT_BAY_TEMPLE_0     0x8C00
-#define ENTRANCE_GREAT_BAY_TEMPLE_1     0x8C10
-#define ENTRANCE_GREAT_BAY_TEMPLE_2     0x8C20
+// #define ENTRANCE_GREAT_BAY_TEMPLE_1     0x8C10 first entrance with turtle cutscene
+// #define ENTRANCE_GREAT_BAY_TEMPLE_2     0x8C20 Crash
 
 // Zora Hall (Ext: 0x30, Ent: 9)
 #define ENTRANCE_ZORA_HALL_0            0x6000
@@ -377,9 +377,9 @@
 #define ENTRANCE_ZORA_HALL_ROOMS_1      0x9210
 #define ENTRANCE_ZORA_HALL_ROOMS_2      0x9220
 #define ENTRANCE_ZORA_HALL_ROOMS_3      0x9230
-#define ENTRANCE_ZORA_HALL_ROOMS_4      0x9240
+// #define ENTRANCE_ZORA_HALL_ROOMS_4      0x9240 Jamming with zora cutscene Bassist?
 #define ENTRANCE_ZORA_HALL_ROOMS_5      0x9250
-#define ENTRANCE_ZORA_HALL_ROOMS_6      0x9260
+// #define ENTRANCE_ZORA_HALL_ROOMS_6      0x9260 Jamming with Keyboard player?
 
 // Marine Research Lab (Ext: 0x2C, Ent: 1)
 #define ENTRANCE_MARINE_RESEARCH_LAB_0  0x5800
@@ -400,8 +400,8 @@
 #define ENTRANCE_IKANA_CANYON_6         0x2060
 #define ENTRANCE_IKANA_CANYON_7         0x2070
 #define ENTRANCE_IKANA_CANYON_8         0x2080
-#define ENTRANCE_IKANA_CANYON_9         0x2090
-#define ENTRANCE_IKANA_CANYON_10        0x20A0
+// #define ENTRANCE_IKANA_CANYON_9         0x2090 Song of storms cave cutscene and music playing from house
+// #define ENTRANCE_IKANA_CANYON_10        0x20A0 Inside cave with Sharp cutscene after using song of storms
 #define ENTRANCE_IKANA_CANYON_11        0x20B0
 #define ENTRANCE_IKANA_CANYON_12        0x20C0
 #define ENTRANCE_IKANA_CANYON_13        0x20D0
@@ -452,7 +452,7 @@
 // Deku Shrine (Ext: 0x4F, Ent: 3)
 #define ENTRANCE_DEKU_SHRINE_0          0x9E00
 #define ENTRANCE_DEKU_SHRINE_1          0x9E10
-#define ENTRANCE_DEKU_SHRINE_2          0x9E20
+// #define ENTRANCE_DEKU_SHRINE_2          0x9E20 Black screen?
 
 // ========================================================================
 // SHOPS & INDOOR LOCATIONS
@@ -483,7 +483,7 @@
 // Curiosity Shop (Ext: 0x07, Ent: 4)
 #define ENTRANCE_CURIOSITY_SHOP_0       0x0E00
 #define ENTRANCE_CURIOSITY_SHOP_1       0x0E10
-#define ENTRANCE_CURIOSITY_SHOP_2       0x0E20
+// #define ENTRANCE_CURIOSITY_SHOP_2       0x0E20 Peeking into back of curiosity shop
 #define ENTRANCE_CURIOSITY_SHOP_3       0x0E30
 
 // Stock Pot Inn (Ext: 0x5E, Ent: 6)
@@ -491,7 +491,7 @@
 #define ENTRANCE_STOCK_POT_INN_1        0xBC10
 #define ENTRANCE_STOCK_POT_INN_2        0xBC20
 #define ENTRANCE_STOCK_POT_INN_3        0xBC30
-#define ENTRANCE_STOCK_POT_INN_4        0xBC40
+// #define ENTRANCE_STOCK_POT_INN_4        0xBC40 ANJU and family cutscene about Kafei running off
 #define ENTRANCE_STOCK_POT_INN_5        0xBC50
 
 // Post Office (Ext: 0x2B, Ent: 1)
@@ -510,7 +510,7 @@
 // Deku King's Chamber (Ext: 0x3B, Ent: 4)
 #define ENTRANCE_DEKU_KINGS_CHAMBER_0   0x7600
 #define ENTRANCE_DEKU_KINGS_CHAMBER_1   0x7610
-#define ENTRANCE_DEKU_KINGS_CHAMBER_2   0x7620
+// #define ENTRANCE_DEKU_KINGS_CHAMBER_2   0x7620 Deku Princess saves Monkey cutscene
 #define ENTRANCE_DEKU_KINGS_CHAMBER_3   0x7630
 
 // ========================================================================
@@ -579,11 +579,11 @@
 #define ENTRANCE_FAIRYS_FOUNTAIN_2      0x4620
 #define ENTRANCE_FAIRYS_FOUNTAIN_3      0x4630
 #define ENTRANCE_FAIRYS_FOUNTAIN_4      0x4640
-#define ENTRANCE_FAIRYS_FOUNTAIN_5      0x4650
-#define ENTRANCE_FAIRYS_FOUNTAIN_6      0x4660
-#define ENTRANCE_FAIRYS_FOUNTAIN_7      0x4670
-#define ENTRANCE_FAIRYS_FOUNTAIN_8      0x4680
-#define ENTRANCE_FAIRYS_FOUNTAIN_9      0x4690
+// #define ENTRANCE_FAIRYS_FOUNTAIN_5      0x4650 Magic Power cutscene
+// #define ENTRANCE_FAIRYS_FOUNTAIN_6      0x4660 mastered Spin Attack cutscene?
+// #define ENTRANCE_FAIRYS_FOUNTAIN_7      0x4670 Magic Power Enhanced
+// #define ENTRANCE_FAIRYS_FOUNTAIN_8      0x4680 Double Defense
+// #define ENTRANCE_FAIRYS_FOUNTAIN_9      0x4690 Great Fairy's Sword
 
 // Astral Observatory (Ext: 0x26, Ent: 3)
 #define ENTRANCE_ASTRAL_OBSERVATORY_0   0x4C00
@@ -593,25 +593,25 @@
 // Cutscene Map (Ext: 0x0E, Ent: 10)
 #define ENTRANCE_CUTSCENE_MAP_0         0x1C00
 #define ENTRANCE_CUTSCENE_MAP_1         0x1C10
-#define ENTRANCE_CUTSCENE_MAP_2         0x1C20
+// #define ENTRANCE_CUTSCENE_MAP_2         0x1C20 CRASH
 #define ENTRANCE_CUTSCENE_MAP_3         0x1C30
-#define ENTRANCE_CUTSCENE_MAP_4         0x1C40
-#define ENTRANCE_CUTSCENE_MAP_5         0x1C50
-#define ENTRANCE_CUTSCENE_MAP_6         0x1C60
-#define ENTRANCE_CUTSCENE_MAP_7         0x1C70
+// #define ENTRANCE_CUTSCENE_MAP_4         0x1C40 CRASH
+// #define ENTRANCE_CUTSCENE_MAP_5         0x1C50 CRASH
+// #define ENTRANCE_CUTSCENE_MAP_6         0x1C60 CRASH
+// #define ENTRANCE_CUTSCENE_MAP_7         0x1C70 CRASH
 #define ENTRANCE_CUTSCENE_MAP_8         0x1C80
 #define ENTRANCE_CUTSCENE_MAP_9         0x1C90
 
 // Before the Portal to Termina (Ext: 0x17, Ent: 5)
-#define ENTRANCE_OPENING_DUNGEON_0      0x2E00
+// #define ENTRANCE_OPENING_DUNGEON_0      0x2E00 Falling into Termina cutscene
 #define ENTRANCE_OPENING_DUNGEON_1      0x2E10
-#define ENTRANCE_OPENING_DUNGEON_2      0x2E20
+// #define ENTRANCE_OPENING_DUNGEON_2      0x2E20 Turned to Deku Scrub cutscene
 #define ENTRANCE_OPENING_DUNGEON_3      0x2E30
-#define ENTRANCE_OPENING_DUNGEON_4      0x2E40
+// #define ENTRANCE_OPENING_DUNGEON_4      0x2E40 First Song of Time played cutscene
 
 // Lost Woods (Ext: 0x62, Ent: 3)
-#define ENTRANCE_LOST_WOODS_0           0xC400
-#define ENTRANCE_LOST_WOODS_1           0xC410
+// #define ENTRANCE_LOST_WOODS_0           0xC400 Dragged by horse and skull kid
+#define ENTRANCE_LOST_WOODS_1           0xC410 
 #define ENTRANCE_LOST_WOODS_2           0xC420
 
 // Woods of Mystery (Ext: 0x61, Ent: 1)
@@ -634,8 +634,8 @@
 
 // Waterfall Rapids (Ext: 0x47, Ent: 4)
 #define ENTRANCE_WATERFALL_RAPIDS_0     0x8E00
-#define ENTRANCE_WATERFALL_RAPIDS_1     0x8E10
-#define ENTRANCE_WATERFALL_RAPIDS_2     0x8E20
+// #define ENTRANCE_WATERFALL_RAPIDS_1     0x8E10 Beaver Race
+// #define ENTRANCE_WATERFALL_RAPIDS_2     0x8E20 End of beaver race, you didn't get all the rings and cheated?
 #define ENTRANCE_WATERFALL_RAPIDS_3     0x8E30
 
 // Doggy Racetrack (Ext: 0x3E, Ent: 2)
@@ -644,7 +644,7 @@
 
 // Cucco Shack (Ext: 0x3F, Ent: 2)
 #define ENTRANCE_CUCCO_SHACK_0          0x7E00
-#define ENTRANCE_CUCCO_SHACK_1          0x7E10
+// #define ENTRANCE_CUCCO_SHACK_1          0x7E10 After marching with cuccos
 
 // Stone Tower (Ext: 0x55, Ent: 4)
 #define ENTRANCE_STONE_TOWER_0          0xAA00
@@ -663,7 +663,7 @@
 // Stone Tower Temple - Inverted (Ext: 0x15, Ent: 3)
 #define ENTRANCE_STONE_TOWER_TEMPLE_INVERTED_0 0x2A00
 #define ENTRANCE_STONE_TOWER_TEMPLE_INVERTED_1 0x2A10
-#define ENTRANCE_STONE_TOWER_TEMPLE_INVERTED_2 0x2A20
+// #define ENTRANCE_STONE_TOWER_TEMPLE_INVERTED_2 0x2A20 CRASH
 
 // Path to Goron Village - Winter (Ext: 0x5A, Ent: 3)
 #define ENTRANCE_PATH_TO_GORON_VILLAGE_WINTER_0 0xB400
@@ -677,7 +677,7 @@
 
 // Goron Racetrack (Ext: 0x68, Ent: 3)
 #define ENTRANCE_GORON_RACETRACK_0      0xD000
-#define ENTRANCE_GORON_RACETRACK_1      0xD010
+// #define ENTRANCE_GORON_RACETRACK_1      0xD010 Goron Race start cutscene
 #define ENTRANCE_GORON_RACETRACK_2      0xD020
 
 // Gorman Track (Ext: 0x67, Ent: 6)
@@ -689,7 +689,7 @@
 #define ENTRANCE_GORMAN_TRACK_5         0xCE50
 
 // Great Bay Cutscene (Ext: 0x5F, Ent: 1)
-#define ENTRANCE_GREAT_BAY_CUTSCENE_0   0xBE00
+// #define ENTRANCE_GREAT_BAY_CUTSCENE_0   0xBE00 Pirates sail into the storm
 
 // Swamp Shooting Gallery (Ext: 0x21, Ent: 1)
 #define ENTRANCE_SWAMP_SHOOTING_GALLERY_0 0x4200
